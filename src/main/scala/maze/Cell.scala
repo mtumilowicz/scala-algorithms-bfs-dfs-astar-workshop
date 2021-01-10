94e9dd5415ddfe240054ebc4ba9282b0bb6dc289
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-01-10 13:16:29 +0100

    namespaces

--- src/main/scala/maze/Cell.scala
+++ src/main/scala/maze/Cell.scala
@@ -3,8 +3,11 @@ package maze
 sealed class Cell(val code: String) {
   def show(): String = code
 }
-case class Empty() extends Cell(" ")
-case class Blocked() extends Cell("X")
-case class Start() extends Cell("S")
-case class Goal() extends Cell("G")
-case class Path() extends Cell("*")
+
+object Cell {
+  case class Empty() extends Cell(" ")
+  case class Blocked() extends Cell("X")
+  case class Start() extends Cell("S")
+  case class Goal() extends Cell("G")
+  case class Path() extends Cell("*")
+}
--- src/main/scala/maze/Maze.scala
+++ src/main/scala/maze/Maze.scala
@@ -18,11 +18,11 @@ case class Maze(
 
   private def generateRandomGrid(sparseness: Double): Array[Array[Cell]] = {
     val r = scala.util.Random
-    val grid: Array[Array[Cell]] = Array.fill(rows, columns)(Empty())
+    val grid: Array[Array[Cell]] = Array.fill(rows, columns)(Cell.Empty())
     for (
       row <- 0 until rows;
       col <- 0 until columns
-    ) if (r.nextDouble() < sparseness) grid(row)(col) = Blocked()
+    ) if (r.nextDouble() < sparseness) grid(row)(col) = Cell.Blocked()
 
     grid
   }
@@ -41,21 +41,21 @@ case class Maze(
 
   def mark(path: List[MazeLocation]): Unit = {
     for (ml <- path) {
-      grid(ml.row)(ml.column) = Path()
+      grid(ml.row)(ml.column) = Cell.Path()
     }
 
-    grid(start.row)(start.column) = Start()
-    grid(goal.row)(goal.column) = Goal()
+    grid(start.row)(start.column) = Cell.Start()
+    grid(goal.row)(goal.column) = Cell.Goal()
   }
 
   def goalTest(ml: MazeLocation): Boolean = goal.equals(ml)
 
   def successors(ml: MazeLocation): List[MazeLocation] = {
     val locations = ListBuffer[MazeLocation]()
-    if (ml.row + 1 < rows && (grid(ml.row + 1)(ml.column) != Blocked())) locations.addOne(MazeLocation(ml.row + 1, ml.column))
-    if (ml.row - 1 >= 0 && (grid(ml.row - 1)(ml.column) != Blocked())) locations.addOne(MazeLocation(ml.row - 1, ml.column))
-    if (ml.column + 1 < columns && (grid(ml.row)(ml.column + 1) != Blocked())) locations.addOne(MazeLocation(ml.row, ml.column + 1))
-    if (ml.column - 1 >= 0 && (grid(ml.row)(ml.column - 1) != Blocked())) locations.addOne(MazeLocation(ml.row, ml.column - 1))
+    if (ml.row + 1 < rows && (grid(ml.row + 1)(ml.column) != Cell.Blocked())) locations.addOne(MazeLocation(ml.row + 1, ml.column))
+    if (ml.row - 1 >= 0 && (grid(ml.row - 1)(ml.column) != Cell.Blocked())) locations.addOne(MazeLocation(ml.row - 1, ml.column))
+    if (ml.column + 1 < columns && (grid(ml.row)(ml.column + 1) != Cell.Blocked())) locations.addOne(MazeLocation(ml.row, ml.column + 1))
+    if (ml.column - 1 >= 0 && (grid(ml.row)(ml.column - 1) != Cell.Blocked())) locations.addOne(MazeLocation(ml.row, ml.column - 1))
     locations.toList
   }
 
