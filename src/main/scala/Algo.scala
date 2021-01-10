1cb7e518eb1f135d6aee4fca58e8489063c811b0
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-01-10 12:44:30 +0100

    algo bfs

--- src/main/scala/Algo.scala
+++ src/main/scala/Algo.scala
@@ -19,4 +19,23 @@ object Algo {
     }
     null
   }
+
+  def bfs[T](initial: T, goalTest: T => Boolean, successors: T => List[T]): Node[T] = {
+    val frontier: mutable.Queue[Node[T]] = mutable.Queue()
+    frontier.enqueue(Node(initial))
+    val explored: mutable.Set[T] = mutable.Set[T]()
+    while (frontier.nonEmpty) {
+      val currentNode = frontier.dequeue()
+      val currentState = currentNode.state
+      if (goalTest(currentState)) return currentNode
+      for (
+        child <- successors(currentState)
+        if !explored.contains(child)
+      ) {
+        explored.add(child)
+        frontier.enqueue(Node(child, currentNode))
+      }
+    }
+    null
+  }
 }
--- src/main/scala/Intro.scala
+++ src/main/scala/Intro.scala
@@ -1,11 +1,20 @@
 object Intro extends App {
-  val m = new Maze()
-  println(m.show())
-  val solution1 = Algo.dfs(m.start, m.goalTest, m.successors)
-  if (solution1 == null) System.out.println("No solution found using depth-first search!")
-  else {
-    val path1 = solution1.toPath()
-    m.mark(path1)
+  run(Algo.dfs)
+
+  println("--------------------------------------")
+
+  run(Algo.bfs)
+
+  def run(algo: (MazeLocation, MazeLocation => Boolean, MazeLocation => List[MazeLocation]) => Node[MazeLocation]): Unit = {
+    val m = new Maze()
     println(m.show())
+    val solution1 = algo(m.start, m.goalTest, m.successors)
+    if (solution1 == null) System.out.println("No solution found using depth-first search!")
+    else {
+      val path1 = solution1.toPath()
+      m.mark(path1)
+      println(m.show())
+    }
   }
+
 }
