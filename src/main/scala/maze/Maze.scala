ff32168659cf464806310798b6561bc05612d66a
mtumilowicz <michal.tumilowicz01@gmail.com>
2021-04-20 21:25:22 +0200

    maze

--- src/main/scala/maze/Maze.scala
+++ src/main/scala/maze/Maze.scala
@@ -51,24 +51,3 @@ case class Maze(
     copy
   }
 }
-
-object Maze {
-  def apply(): Maze = {
-    Maze(
-      generateRandomGrid(10, 10, 0.2),
-      start = Location(0, 0),
-      goal = Location(9, 9),
-    )
-  }
-
-  private def generateRandomGrid(rows: Int, cols: Int, sparseness: Double): Array[Array[Cell]] = {
-    val r = scala.util.Random
-    val grid: Array[Array[Cell]] = Array.fill(rows, cols)(Cell.Empty())
-    for (
-      row <- 0 until rows;
-      col <- 0 until cols
-    ) if (r.nextDouble() < sparseness) grid(row)(col) = Cell.Blocked()
-
-    grid
-  }
-}
